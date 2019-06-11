8ab38b2787e1cef09a9a230858a55bbfd95eb426
Jure Cuhalev <jure@cuhalev.com>
2019-06-11 20:03:37 +0200

    Implements screenshots via capture-website-cli

--- badges/main/views.py
+++ badges/main/views.py
@@ -5,6 +5,8 @@ from django.template import Template, Context
 
 import toml
 from pathlib import PurePath
+import sarge
+import magic
 
 from .models import BadgeTemplate
 
@@ -16,14 +18,14 @@ class PreviewIndexView(View):
             "TEMPLATE_SLUG": self.badge_template.slug,
         }
 
-        config = toml.loads(
-            open(PurePath(self.badge_template.repository, "config.toml")).read()
-        )
-        sample_data = config["sample-data"]
-
+        sample_data = self.config["sample-data"]
         context["data"] = sample_data
 
         return context
+
+    @property
+    def config(self):
+        return toml.load(PurePath(self.badge_template.repository, "config.toml"))
 
     @property
     def filename(self):
@@ -36,9 +38,39 @@ class PreviewIndexView(View):
     def badge_template(self):
         return BadgeTemplate.objects.get(slug=self.kwargs["slug"])
 
-    def get(self, request, *args, **kwargs):
-        source = open(PurePath(self.badge_template.repository, self.filename)).read()
-        t = Template(source)
-        c = Context(self.get_context_data())
+    @property
+    def render_config(self):
+        for template_file in self.config["files"]:
+            if self.filename == template_file["filename"]:
+                return template_file
 
-        return HttpResponse(t.render(c), request)
+    def get_render(self):
+        cmd = (
+            "capture-website {url} --width={width} --height={height} "
+            "--type={type} --element={element} --no-default-background".format(
+                width=self.render_config.get("screen_width", 1000),
+                height=self.render_config.get("screen_height", 1000),
+                type=self.render_config.get("format", "png"),
+                element=sarge.shell_quote(
+                    self.render_config.get("element", ".screenshot")
+                ),
+                url="http://localhost:8000/preview/oeawards/single-badge.html",
+            )
+        )
+        p = sarge.run(cmd, stdout=sarge.Capture())
+
+        image = p.stdout.bytes
+        mime = magic.from_buffer(image, mime=True)
+        return HttpResponse(image, content_type=mime)
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get("render", False):
+            return self.get_render()
+        else:
+            source = open(
+                PurePath(self.badge_template.repository, self.filename)
+            ).read()
+            t = Template(source)
+            c = Context(self.get_context_data())
+
+            return HttpResponse(t.render(c), request)
